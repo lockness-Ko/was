8896947d0ed8f9868954e794d25bd42072334df9
lockness-Ko <videolk9373@gmail.com>
2024-01-15 18:30:30 +1100

    (update) add more radios!

--- was.sh
+++ was.sh
@@ -5,7 +5,7 @@ echo -e "$INFO Welcome to Wi-Fi Attack Sim v0.1.0!"
 
 # Load mac80211_hwsim
 echo -e "$INFO Checking for mac80211_hwsim presence"
-sudo lsmod | grep "mac80211_hwsim" && echo -n || bash -c "echo -e \"$WARN mac80211_hwsim not loaded. Loading...\"; sudo modprobe mac80211_hwsim radios=4"
+sudo lsmod | grep "mac80211_hwsim" && echo -n || bash -c "echo -e \"$WARN mac80211_hwsim not loaded. Loading...\"; sudo modprobe mac80211_hwsim radios=5"
 echo -e "$INFO Loaded mac80211_hwsim"
 
 . ./scripts/functions.sh
